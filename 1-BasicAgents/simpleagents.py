004d77ea1ca57d2555604786adef376b748d10a9
SANNNNN-123 <zuhairaziz95@yahoo.com>
2025-04-15 02:58:49 +0800

    intial

---
+++ 1-BasicAgents/simpleagents.py
@@ -0,0 +1,9 @@
+from agno.agent import Agent, Task, AgentExecutor
+from agno.tools import DuckDuckGoSearchRun
+from agno.tools import SerperDevTool
+from agno.tools import WebBaseLoader
+from agno.tools import WebBaseLoader
+from agno.tools import WebBaseLoader
+
+
+
